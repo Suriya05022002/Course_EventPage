21e9b7aa1a56d63d295758e503507f7e1ed420ce
Suriya Narayanan G <113960936+Suriya05022002@users.noreply.github.com>
2025-08-21 13:52:45 +0530

    script.js

--- script.js
+++ script.js
@@ -844,14 +844,9 @@ function updateResultsCount() {
 
 // Format date
 function formatDate(dateString) {
-  const date = new Date(dateString)
-  return date
-    .toLocaleDateString("en-US", {
-      weekday: "short",
-      day: "numeric",
-      year: "numeric",
-    })
-    .replace(/(\w+), (\d+), (\d+)/, "$1, $2, $3")
+  const date = new Date(dateString);
+  const options = { day: '2-digit', month: 'short', year: 'numeric' };
+  return date.toLocaleDateString('en-GB', options).replace(/ /g, ' ');
 }
 
 // Show course details in modal
